fa7f5d41d7aea7e5ce0b80afa918ca1fffd8c54e
krithikkr98 <krithikkr98@gmail.com>
2022-03-10 17:39:49 +0530

    new

---
+++ DELETE.js
@@ -0,0 +1,29 @@
+var AWS = require("aws-sdk");
+
+let dynamo = new AWS.DynamoDB.DocumentClient();
+
+let userId = "user11Test11"
+http://3.111.142.245:8080/job/github-aws/
+let remove = function () {
+
+    if(!userId){
+        console.log("user::parameter_missing::bad_request")
+    }
+
+    var params = {
+        TableName: "user",
+        Key: {
+            "user_id": userId
+        }
+    };
+    dynamo.delete(params, function (err, data) {
+
+        if (err) {
+            console.log("user::delete::error - " + JSON.stringify(err, null, 2));
+        } else {
+            console.log("user::delete::success");
+        }
+    });
+}
+
+remove();
